4d945210b86d1b6aa69c4757738ccfead7d292c0
Antti Hurme <zimeon@null-byte.org>
2025-08-26 14:09:21 +0300

    Update find_tags.py

--- find_tags.py
+++ find_tags.py
@@ -1,12 +1,17 @@
+#!/home/ruuvi/Ruuvitag/venv/bin/python3
 """
 Find RuuviTags
 """
-
+import asyncio
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 import ruuvitag_sensor.log
 
 ruuvitag_sensor.log.enable_console()
 
-# This will print sensor's mac and state when new sensor is found
+async def main():
+    sensors = await RuuviTagSensor.find_ruuvitags_async()
+    for sensor in sensors:
+        print(sensor.mac, sensor.update, sensor.data)
+
 if __name__ == "__main__":
-    RuuviTagSensor.find_ruuvitags()
+    asyncio.run(main())
